faf60f54c2b53716994e66e5d3e215ed229701e8
Leonid Krapchatov <hellsing1998nsk@gmail.com>
2024-03-02 18:30:12 +0700

    Add shop model

--- Ecom_admin_panel/admin_panel/models.py
+++ Ecom_admin_panel/admin_panel/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+class Shop(models.Model):
+    title = models.CharField("Название", max_length=300)
+    description = models.CharField("Описание", max_length=1000)
+    imageUrl = models.URLField("Ссылка на изображение")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Магазин"
+        verbose_name = "Магазины"
